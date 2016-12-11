d3167643a57a7b8988f787b1771bc04c4c547e2d
thetwentyseven <adrian.vcch@gmail.com>
2016-12-11 20:21:36 +0000

    Backup 3

--- index.php
+++ index.php
@@ -38,6 +38,9 @@
           I was raised in a special government facility managed by the “fairy”, an AI system, which aims to produce the perfect human race.
           The system taught me everything but I always knew there was something missing. Until one day…
         </p>
+        <audio controls>
+          <source src="tracks/2_with_audio.mp3" type="audio/mp3">
+        </audio>
         <div class="next">
           <a href="#chapter-three" class="button button_2">Next</a>
         </div>
@@ -50,6 +53,9 @@
           What I did not know was that he was undercover fighting against this cruel inhumane system.
           I had found what was missing in my life. I had fallen in love with him.
         </p>
+        <audio controls>
+          <source src="tracks/3_with_audio.mp3" type="audio/mp3">
+        </audio>
         <div class="next">
           <a href="#chapter-four" class="button button_3">Next</a>
         </div>
@@ -61,6 +67,9 @@
           We found a way to let him in to the cell, which could only be opened from outside with my DNA sample.
           That’s why I had to put a piece of my long blond hair into the ventilation system so he could use to access the cell.
         </p>
+        <audio controls>
+          <source src="tracks/4_with_audio.mp3" type="audio/mp3">
+        </audio>
         <div class="next">
           <a href="#chapter-five" class="button button_4">Next</a>
         </div>
@@ -73,6 +82,9 @@
           out of the facility to an unknown place. The other humans called it the “Ghetto”. This is the place where
           the unwanted humans and imperfect children lived
         </p>
+        <audio controls>
+          <source src="tracks/5_with_audio.mp3" type="audio/mp3">
+        </audio>
         <div class="next">
           <a href="#chapter-six" class="button button_5">Next</a>
         </div>
@@ -84,6 +96,9 @@
           Fairy tricked my love by using the hair that was cut from me. When he entered the cell, he was captured and then
           tortured by robots for months. Barely alive, the “fairy” sent him into the “Ghetto” where I was.
         </p>
+        <audio controls>
+          <source src="tracks/6_with_audio.mp3" type="audio/mp3">
+        </audio>
         <div class="next">
           <a href="#chapter-seven" class="button button_6">Next</a>
         </div>
@@ -91,9 +106,14 @@
 
       <!-- chapter-seven -->
       <div class="chapter" id="chapter-seven">
-        <p>One day, in the deepest corner of the Ghetto, I found him lying on the ground.
+        <p>
+          One day, in the deepest corner of the Ghetto, I found him lying on the ground.
           For months, I treated his wounds. We were finally back together but we knew there were more people to rescue.
-          That´s why we chose to fight back.</p>
+          That´s why we chose to fight back.
+        </p>
+        <audio controls>
+          <source src="tracks/7_with_audio.mp3" type="audio/mp3">
+        </audio>
       </div>
 
   </div>
